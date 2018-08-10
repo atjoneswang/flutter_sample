016f4a1134deb9bdd8a0d542771fe4308f9a4736
Jones Wang <jones.wang@gsit.com.tw>
2018-08-10 15:46:11 +0800

    update menu ui layout

--- clapanimation/lib/menupage.dart
+++ clapanimation/lib/menupage.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'list_menu.dart';
 import 'common/widgetmods.dart';
 
 class MainPage extends StatelessWidget {
   final String title;
-
   MainPage(this.title);
 
   @override
   Widget build(BuildContext context) {
     final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Container(
-      decoration: buildRadiusContainer(),
-      margin: EdgeInsets.only(
-            top: statusBarHeight+4, bottom: 12.0, left: 10.0, right: 10.0),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            title,
-            style: TextStyle(fontSize: 16.0, color: Colors.black87),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.blue[200], Colors.blueGrey[600]]),
+      ),
+      child: Container(
+        decoration: buildRadiusContainer(),
+        margin: EdgeInsets.only(
+            top: statusBarHeight + 4, bottom: 12.0, left: 10.0, right: 10.0),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              title,
+              style: TextStyle(fontSize: 16.0, color: Colors.black87),
+            ),
+            centerTitle: true,
+            elevation: 0.0,
+            backgroundColor: Colors.white,
+            brightness: Brightness.dark,
           ),
-          centerTitle: true,
-          elevation: 0.0,
+          body: MenuList(),
           backgroundColor: Colors.white,
-          brightness: Brightness.light,
         ),
-        body: MenuList(),
-        backgroundColor: Colors.white,
       ),
     );
   }
 }
-
