ec06c5b3bf1b09f486928f13823dc04cd018a8fb
Jones Wang <jones.wang@gsit.com.tw>
2018-08-20 18:21:30 +0800

    update animation

--- clapanimation/lib/pagereveal/page_indicator.dart
+++ clapanimation/lib/pagereveal/page_indicator.dart
@@ -12,29 +12,37 @@ class PageIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<PageBubble> bubbles = [];
-    for(var i = 0; i < viewModel.pages.length; ++i) {
+    for (var i = 0; i < viewModel.pages.length; ++i) {
       var page = viewModel.pages[i];
 
       var percentActive;
-      if(i == viewModel.activeIndex){
+      if (i == viewModel.activeIndex) {
         percentActive = 1.0 - viewModel.slidePercent;
-      } else if (i == viewModel.activeIndex - 1 && viewModel.direction == SlideDirection.leftToRight) {
+      } else if (i == viewModel.activeIndex - 1 &&
+          viewModel.direction == SlideDirection.leftToRight) {
         percentActive = viewModel.slidePercent;
-      } else if (i == viewModel.activeIndex + 1 && viewModel.direction == SlideDirection.rightToLeft) {
+      } else if (i == viewModel.activeIndex + 1 &&
+          viewModel.direction == SlideDirection.rightToLeft) {
         percentActive = viewModel.slidePercent;
       } else {
         percentActive = 0.0;
       }
-      bool isHollow = i >= viewModel.activeIndex;
+      bool isHollow = i > viewModel.activeIndex ||
+          (i == viewModel.activeIndex &&
+              viewModel.direction == SlideDirection.leftToRight);
 
-      bubbles.add(
-        new PageBubble(
+      bubbles.add(new PageBubble(
         viewModel: new PageBubbleViewModel(
-          page.iconAssetIcon, 
-          page.color, 
-          isHollow, 
-          percentActive),
+            page.iconAssetIcon, page.color, isHollow, percentActive),
       ));
+    }
+    final BUBBLE_WIDTH = 55.0;
+    final baseTranslation = ((viewModel.pages.length * BUBBLE_WIDTH) / 2) - (BUBBLE_WIDTH /2);
+    var translation = baseTranslation - (viewModel.activeIndex * BUBBLE_WIDTH);
+    if(viewModel.direction == SlideDirection.leftToRight) {
+      translation += BUBBLE_WIDTH * viewModel.slidePercent;
+    }else if(viewModel.direction == SlideDirection.rightToLeft) {
+      translation -= BUBBLE_WIDTH * viewModel.slidePercent;
     }
 
     return Column(
@@ -42,9 +50,12 @@ class PageIndicator extends StatelessWidget {
         Expanded(
           child: Container(),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: bubbles,
+        Transform(
+          transform: new Matrix4.translationValues(translation, 0.0, 0.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: bubbles,
+          ),
         ),
       ],
     );
@@ -63,7 +74,8 @@ class PagerIndicatorViewModel {
   final double slidePercent;
   final SlideDirection direction;
 
-  PagerIndicatorViewModel(this.pages, this.activeIndex, this.direction, this.slidePercent);
+  PagerIndicatorViewModel(
+      this.pages, this.activeIndex, this.direction, this.slidePercent);
 }
 
 class PageBubble extends StatelessWidget {
@@ -73,27 +85,35 @@ class PageBubble extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: new Container(
-        width: ui.lerpDouble(20.0, 45.0, viewModel.activePercent),
-        height: ui.lerpDouble(20.0, 45.0, viewModel.activePercent),
-        decoration: new BoxDecoration(
-              color: viewModel.isHollow 
-              ? const Color(0x88ffffff).withAlpha((0x88 * viewModel.activePercent).round())
-              : const Color(0x88ffffff),
+    print('build bubble');
+    return new Container(
+      width: 55.0,
+      height: 65.0,
+      child: new Center(
+        child: new Container(
+          width: ui.lerpDouble(20.0, 45.0, viewModel.activePercent),
+          height: ui.lerpDouble(20.0, 45.0, viewModel.activePercent),
+          decoration: new BoxDecoration(
+            shape: BoxShape.circle,
+              color: viewModel.isHollow
+                  ? const Color(0x88ffffff)
+                      .withAlpha((0x88 * viewModel.activePercent).round())
+                  : const Color(0x88ffffff),
               border: Border.all(
-                color: viewModel.isHollow
-                ? const Color(0x88ffffff).withAlpha((0x88 * (1.0 - viewModel.activePercent)).round())
-                : Colors.transparent,
-              width: 3.0
+                  color: viewModel.isHollow
+                      ? const Color(0x88ffffff).withAlpha(
+                          (0x88 * (1.0 - viewModel.activePercent)).round())
+                      : Colors.transparent,
+                  width: 3.0
+                  ),
               ),
-              shape: BoxShape.circle),
-        child: Opacity(
-            opacity: viewModel.activePercent,
-            child:
-                new Image.asset(viewModel.iconAssetPath, color: viewModel.color)),
+          child: Opacity(
+              opacity: viewModel.activePercent,
+              child: new Image.asset(viewModel.iconAssetPath,
+                  color: viewModel.color)),
+        ),
       ),
+      
     );
   }
 }
--- clapanimation/lib/pagereveal/pagedemo.dart
+++ clapanimation/lib/pagereveal/pagedemo.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:clipanimation/pagereveal/page_dragger.dart';
 import 'package:clipanimation/pagereveal/page_indicator.dart';
 import 'package:clipanimation/pagereveal/page_reveal.dart';
 import 'package:clipanimation/pagereveal/pages.dart';
@@ -8,30 +11,94 @@ class PageRevealDemo extends StatefulWidget {
   _PageRevealDemoState createState() => _PageRevealDemoState();
 }
 
-class _PageRevealDemoState extends State<PageRevealDemo> {
+class _PageRevealDemoState extends State<PageRevealDemo> with TickerProviderStateMixin {
+  StreamController<SlideUpdate> slideUpdateStream;
+  AnimatedPageDragger animatedPageDragger;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _PageRevealDemoState() {
+    slideUpdateStream = new StreamController<SlideUpdate>();
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.doneDragging) {
+          print('Done dragging');
+          if (slidePercent > 0.5) {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.open,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this
+            );
+          }else{
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.close,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this
+            );
+            nextPageIndex = activeIndex;
+          }
+          animatedPageDragger.run();
+          
+        } else if (event.updateType == UpdateType.dragging) {
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if(slideDirection == SlideDirection.leftToRight){
+            nextPageIndex = activeIndex -1;
+          }else if(slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          }else{
+            nextPageIndex = activeIndex;
+          }
+        }else if(event.updateType == UpdateType.animating) {
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+        
+        }else if(event.updateType == UpdateType.doneAnimating){
+          print('Done animating. Next page index: $nextPageIndex');
+          activeIndex = nextPageIndex;
+          
+          slideDirection = SlideDirection.none;
+          slidePercent == 0.0;
+          animatedPageDragger.dispose();
+        }
+        print('active index: $activeIndex');
+        print('next index: $nextPageIndex');
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           Page(
-            viewModel: pages[0],
+            viewModel: pages[activeIndex],
             percentVisible: 1.0,
           ),
           PageReveal(
-            revealPercent: 0.2,
+            revealPercent: slidePercent,
             child: Page(
-              viewModel: pages[1],
-              percentVisible: 1.0,
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
             ),
           ),
           PageIndicator(
             viewModel: new PagerIndicatorViewModel(
-              pages, 
-              1, 
-              SlideDirection.rightToLeft, 
-              1.0
-            ),
+                pages, activeIndex, slideDirection, slidePercent),
+          ),
+          PageDagger(
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            canDragLeftToRight: activeIndex > 0,
+            slideUpdateStream: slideUpdateStream,
           ),
         ],
       ),
